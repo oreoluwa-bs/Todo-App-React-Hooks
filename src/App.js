7b92d384d18860c123ecdde1335819227cc4706a
Oreoluwa Bimbo-Salami <oreoluwabs25@gmail.com>
2019-09-09 11:56:23 +0100

    Fin

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import uuid from 'uuid/v4';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [inputText, setInputText] = useState('');
+
+  const handleChange = (e) => {
+    setInputText(e.target.value);
+  }
+
+  const handleSubmit = () => {
+    if (inputText !== '') {
+      setTodos([...todos, { _id: uuid(), text: inputText, isCompleted: false }]);
+      setInputText('');
+    }
+  }
+  const handlePress = (event) => {
+    if (event.key === 'Enter') {
+      handleSubmit();
+    }
+  }
+  useEffect(() => {
+    console.log(todos);
+  }, [todos]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className="header">
+        <input type="text" className="textInput" value={inputText} placeholder="Enter an Activity..."
+          onChange={(e) => handleChange(e)}
+          onKeyPress={handlePress.bind(this)} />
+
+        <div className="btn" onClick={handleSubmit}>+</div>
+
+      </div>
+      <div className='container'>
+        <ul>
+          {
+            todos.map(todo => {
+              return (
+                <li key={todo._id} className="note">
+                  {todo.text}
+                  <div className="buttons">
+                    <div className="remove" onClick={() => {
+                      let newTodo = [...todos];
+                      newTodo = newTodo.filter(ntodo => ntodo._id !== todo._id);
+                      setTodos(newTodo);
+                    }}>
+                      Delete
+                    </div>
+
+                    <div className="completeDiv" onClick={() => {
+                      let newTodo = [...todos];
+                      let index = newTodo.findIndex(ntodo => ntodo._id === todo._id);
+                      newTodo[index].isCompleted = !newTodo[index].isCompleted;
+                      setTodos(newTodo);
+                    }
+                    }>
+                      {todo.isCompleted ? <span className='complete'>Complete</span> : <span className=''>Not Complete</span>}
+                    </div>
+                  </div>
+                </li>
+              )
+            })
+          }
+        </ul>
+      </div>
     </div>
   );
 }
